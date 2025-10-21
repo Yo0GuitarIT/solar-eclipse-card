f1b91b8281efdaaaf8bce34ab5e82d4421641bf0
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-10-22 01:56:58 +0800

    fix: 更新音樂連結和圖片路徑，調整內容區域的間距

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,24 +17,37 @@ export default function Home() {
       name: "Spotify",
       imgSrc: "https://services.linkfire.com/logo_spotify_onlight.svg",
       musicLink:
-        "https://open.spotify.com/artist/4tREKMNVSLyiI0K6IhgHVc?si=de1dP930T6e_9uVG2s_IYw",
-    },
-    {
-      name: "YouTube Music",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/commons/0/04/YouTube_Music_logo.svg",
-      musicLink:
-        "https://music.youtube.com/channel/UCVY1s-SKQrfCnZPWXAYvm-A?si=BnkdaM_gfc-23w36",
+        "https://open.spotify.com/album/3b0WhPepXHm3K1YT40cN1y?si=Bi-djGF-RKydYv4Btk8X7Q",
     },
     {
       name: "Apple Music",
       imgSrc: "https://services.linkfire.com/logo_applemusic_onlight.svg",
       musicLink:
-        "https://music.apple.com/tw/artist/%E7%99%BD%E9%A0%BB%E7%8E%87/1428600777",
+        "https://music.apple.com/tw/album/%E6%97%A5%E5%85%A8%E8%9D%95-ep/1847322570",
     },
     {
-      name: "kkbox",
+      name: "KKBox",
       imgSrc: "https://www.kkbox.com/about/img/logo/logo.svg",
-      musicLink: "https://kkbox.fm/op0DiX",
+      musicLink: "https://kkbox.fm/KaJzRh",
+    },
+    {
+      name: "YouTube Music",
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/0/04/YouTube_Music_logo.svg",
+      musicLink:
+        "https://music.youtube.com/playlist?list=OLAK5uy_lONO9pGj2MsU-8W8sBvZZ_-aCY8vOAk40&si=VBjAjH9-q4ErGXuD",
+    },
+    {
+      name: "StreetVoice",
+      imgSrc:
+        "https://streetvoice.com/pages/mobile-app/static/images/logo-sv-b-s.svg",
+      musicLink:
+        "https://streetvoice.com/whitefrequency2016/songs/album/97324127/",
+    },
+    {
+      name: "WangYiYun",
+      imgSrc: "https://services.linkfire.com/logo_netease_onlight.svg",
+      musicLink: "https://music.163.com/#/album?id=287789645",
     },
   ];
 
@@ -48,7 +61,7 @@ export default function Home() {
         <div className="absolute inset-0 bg-black/30 backdrop-blur-md"></div>
 
         {/* 內容區域 */}
-        <div className="relative z-10 flex flex-col items-center gap-8">
+        <div className="relative z-10 flex flex-col items-center gap-4">
           <div className="border  overflow-hidden flex items-center justify-center">
             <Image
               src="/albumCover.jpeg"
@@ -59,8 +72,12 @@ export default function Home() {
           </div>
           <Card className="w-full min-w-[350px]">
             <CardHeader>
-              <CardTitle className="text-center">日全蝕 Solar Eclipse</CardTitle>
-              <CardDescription className="text-center">White Frequency</CardDescription>
+              <CardTitle className="text-center">
+                日全蝕 Solar Eclipse
+              </CardTitle>
+              <CardDescription className="text-center">
+                White Frequency
+              </CardDescription>
             </CardHeader>
 
             <CardContent>
@@ -95,8 +112,8 @@ export default function Home() {
               <Image
                 src="/LOGO.png"
                 alt="logoImg"
-                width={100}
-                height={100}
+                width={120}
+                height={120}
                 className="object-cover object-center"
                 style={{ objectPosition: "center center" }}
               />
