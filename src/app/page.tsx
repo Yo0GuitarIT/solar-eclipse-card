687549b3b180d30fa4a19d247e708a4a5fb6c78d
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-10-24 21:24:49 +0800

    fix: 優化封面圖片的導入方式，統一使用變數管理音樂列表

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,55 +12,57 @@ import { Separator } from "@/components/ui/separator";
 
 import Link from "next/link";
 
-export default function Home() {
-  const MusicList = [
-    {
-      name: "Spotify",
-      imgSrc: "https://services.linkfire.com/logo_spotify_onlight.svg",
-      musicLink:
-        "https://open.spotify.com/album/3b0WhPepXHm3K1YT40cN1y?si=Bi-djGF-RKydYv4Btk8X7Q",
-    },
-    {
-      name: "Apple Music",
-      imgSrc: "https://services.linkfire.com/logo_applemusic_onlight.svg",
-      musicLink:
-        "https://music.apple.com/tw/album/%E6%97%A5%E5%85%A8%E8%9D%95-ep/1847322570",
-    },
-    {
-      name: "KKBox",
-      imgSrc: "https://www.kkbox.com/about/img/logo/logo.svg",
-      musicLink: "https://kkbox.fm/KaJzRh",
-    },
-    {
-      name: "YouTube Music",
-      imgSrc:
-        "https://upload.wikimedia.org/wikipedia/commons/0/04/YouTube_Music_logo.svg",
-      musicLink:
-        "https://music.youtube.com/playlist?list=OLAK5uy_lONO9pGj2MsU-8W8sBvZZ_-aCY8vOAk40&si=VBjAjH9-q4ErGXuD",
-    },
-    {
-      name: "StreetVoice",
-      imgSrc:
-        "https://streetvoice.com/pages/mobile-app/static/images/logo-sv-b-s.svg",
-      musicLink:
-        "https://streetvoice.com/whitefrequency2016/songs/album/97324127/",
-    },
-    {
-      name: "Line Music",
-      imgSrc: "https://cdn.worldvectorlogo.com/logos/line-music.svg",
-      musicLink: "https://music-tw.line.me/album/7787184",
-    },
-    {
-      name: "WangYiYun",
-      imgSrc: "https://services.linkfire.com/logo_netease_onlight.svg",
-      musicLink: "https://music.163.com/#/album?id=287789645",
-    },
-  ];
+const coverImageUrl = "/albumCover.jpeg";
 
+const MusicList = [
+  {
+    name: "Spotify",
+    imgSrc: "https://services.linkfire.com/logo_spotify_onlight.svg",
+    musicLink:
+      "https://open.spotify.com/album/3b0WhPepXHm3K1YT40cN1y?si=Bi-djGF-RKydYv4Btk8X7Q",
+  },
+  {
+    name: "Apple Music",
+    imgSrc: "https://services.linkfire.com/logo_applemusic_onlight.svg",
+    musicLink:
+      "https://music.apple.com/tw/album/%E6%97%A5%E5%85%A8%E8%9D%95-ep/1847322570",
+  },
+  {
+    name: "KKBox",
+    imgSrc: "https://www.kkbox.com/about/img/logo/logo.svg",
+    musicLink: "https://kkbox.fm/KaJzRh",
+  },
+  {
+    name: "YouTube Music",
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/0/04/YouTube_Music_logo.svg",
+    musicLink:
+      "https://music.youtube.com/playlist?list=OLAK5uy_lONO9pGj2MsU-8W8sBvZZ_-aCY8vOAk40&si=VBjAjH9-q4ErGXuD",
+  },
+  {
+    name: "StreetVoice",
+    imgSrc:
+      "https://streetvoice.com/pages/mobile-app/static/images/logo-sv-b-s.svg",
+    musicLink:
+      "https://streetvoice.com/whitefrequency2016/songs/album/97324127/",
+  },
+  {
+    name: "Line Music",
+    imgSrc: "https://cdn.worldvectorlogo.com/logos/line-music.svg",
+    musicLink: "https://music-tw.line.me/album/7787184",
+  },
+  {
+    name: "WangYiYun",
+    imgSrc: "https://services.linkfire.com/logo_netease_onlight.svg",
+    musicLink: "https://music.163.com/#/album?id=287789645",
+  },
+];
+
+export default function Home() {
   return (
     <div
       className="bg-black/40 w-full flex justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url(/albumCover.jpeg)" }}
+      style={{ backgroundImage: `url(${coverImageUrl})` }}
     >
       <div className=" relative w-full  flex flex-col items-center min-h-screen gap-8 p-8 bg-cover bg-center bg-no-repeat">
         {/* 灰色濾鏡覆蓋層 */}
@@ -68,9 +70,9 @@ export default function Home() {
 
         {/* 內容區域 */}
         <div className="relative z-10 flex flex-col items-center gap-4">
-          <div className="border  overflow-hidden flex items-center justify-center">
+          <div className="border overflow-hidden flex items-center justify-center">
             <Image
-              src="/albumCover.jpeg"
+              src={coverImageUrl}
               alt="Solar Eclipse"
               width={150}
               height={150}
